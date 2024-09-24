fd8b84db5d966c213f3e4a1a353c3fa5f3e80c6e
Salim-amir <salimamir2005@gmail.com>
2024-09-24 15:06:45 +0700

    modifikasi

---
+++ Modifikasi16.java
@@ -0,0 +1,8 @@
+public class Modifikasi16 {
+    public static void main(String[] args) {
+        double angka = 2;
+       String hasil;
+       hasil = (angka % 2 == 0) ? "Bilangan genap" : "Bilangan ganjil";
+       System.out.println(angka + "adalah" + hasil);
+    }
+}
